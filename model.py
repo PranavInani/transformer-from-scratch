384c3b82538730e9685742ea7753d0d26cab861d
pranavinani <136845154+PranavInani@users.noreply.github.com>
2025-01-14 23:07:11 +0530

    added super init in feed forward block

--- model.py
+++ model.py
@@ -51,6 +51,7 @@ class LayerNormalization(nn.Module):
 
 class FeedForwardBlock(nn.Module):
     def __init__(self, d_model: int, d_ff : int, dropout : float):
+        super().__init__()
         self.linear_1 = nn.Linear(d_model, d_ff)
         self.dropout = nn.Dropout(dropout)
         self.linear_2 = nn.Linear(d_ff, d_model)
