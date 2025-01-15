e391642bcdedf5ddcc449195a4eb7ebb4010c49c
pranavinani <136845154+PranavInani@users.noreply.github.com>
2025-01-15 15:54:37 +0530

    revert back

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 from torch.optim.lr_scheduler import LambdaLR
-from torch.amp import GradScaler, autocast
 
 import warnings
 from tqdm import tqdm
@@ -198,7 +197,6 @@ def train_model(config):
     writer = SummaryWriter(config['experiment_name'])
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
-    scaler = GradScaler(device='cuda')
 
     # If the user specified a model to preload before training, load it
     initial_epoch = 0
@@ -227,9 +225,6 @@ def train_model(config):
         if device == 'cuda':
             print(f"Device name: {torch.cuda.get_device_name(device.index)}")
             print(f"Device memory: {torch.cuda.get_device_properties(device.index).total_memory / 1024 ** 3} GB")
-            if torch.cuda.device_count() > 1:
-                print(f"Using {torch.cuda.device_count()} GPUs")
-                model = torch.nn.DataParallel(model)
         elif device == 'mps':
             print(f"Device name: <mps>")
         else:
@@ -249,29 +244,27 @@ def train_model(config):
             encoder_mask = batch['encoder_mask'].to(device) # (B, 1, 1, seq_len)
             decoder_mask = batch['decoder_mask'].to(device) # (B, 1, seq_len, seq_len)
 
-            with autocast(device_type='cuda'):
-                # Run the tensors through the encoder, decoder and the projection layer
-                encoder_output = model.encode(encoder_input, encoder_mask) # (B, seq_len, d_model)
-                decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # (B, seq_len, d_model)
-                proj_output = model.project(decoder_output) # (B, seq_len, vocab_size)
+            # Run the tensors through the encoder, decoder and the projection layer
+            encoder_output = model.encode(encoder_input, encoder_mask) # (B, seq_len, d_model)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # (B, seq_len, d_model)
+            proj_output = model.project(decoder_output) # (B, seq_len, vocab_size)
 
-                # Compare the output with the label
-                label = batch['label'].to(device) # (B, seq_len)
+            # Compare the output with the label
+            label = batch['label'].to(device) # (B, seq_len)
 
-                # Compute the loss using a simple cross entropy
-                loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
-                batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+            # Compute the loss using a simple cross entropy
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
 
             # Log the loss
             writer.add_scalar('train loss', loss.item(), global_step)
             writer.flush()
 
             # Backpropagate the loss
-            scaler.scale(loss).backward()
+            loss.backward()
 
             # Update the weights
-            scaler.step(optimizer)
-            scaler.update()
+            optimizer.step()
             optimizer.zero_grad(set_to_none=True)
 
             global_step += 1
